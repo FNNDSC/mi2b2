9432a1526f1af1ec48c4b151869224d05937f57d
Jorge <jbernal0019@yahoo.es>
2015-02-23 12:45:59 -0500

    using sortable instead of draggable/droppable

--- viewer.js
+++ viewer.js
@@ -3,126 +3,71 @@
 var xpos;
 var numRenderDivs = 0;
 
-// ui-draggable CSS class is added to movable elems and ui-draggable-dragging is
-// added to elem being moved
-$('#viewthumbnail img').draggable ({
+// ui-sortable CSS class is by default added to the element
+// element being moved is assigned the ui-sortable-helper class
+$('#viewthumbnail').sortable({
   cursor: 'pointer',
-  //scroll: false,
-  scope: 'viewrenders', // restrict drop only on items that have the same scope str
-  revert: 'invalid', // returns if dropped on an element that does not accept it
-  axis: 'x', // displacement only possible in x (horizontal) direction
   containment: '#viewercontainer', // within which the displacement takes place
   helper: 'clone',
   //zIndex: 999,
   appendTo: '#viewrenders',
+  connectWith: ".sortable",
+  dropOnEmpty: true,
+  /*start: function(event, ui) {
+      item = ui.item;
+      newList = oldList = ui.item.parent().parent();
+  },*/
+  beforeStop: function(event, ui) {
+    //alert("Moved " + item.text() + " from " + oldList.attr('id') + " to " + newList.attr('id'));
 
-  //event handlers
-  // ui.helper is the jQuery obj of the dragged elem
-  start: function(evt, ui) {
-    xpos = ui.position.left;
-    ypos = ui.position.top;
-    var j = $(this);
-    var h = j.css("height");
-    var w = j.css("width");
-    ui.helper.css( {height: h, width: w} );
-  },
-
-  drag: function(evt, ui) {
-
-  },
-
-  stop: function(evt, ui) {
-    //this.style.display="";
+    if (ui.placeholder.parent().attr("id") === "viewrenders") {
+      $(this).sortable("cancel");
+      if (numRenderDivs<4) {
+        ++numRenderDivs;
+        var id = 'viewrender2D' + ui.item.css({ display:"none"}).attr("id").replace("img","");
+        $('#viewrenders').append(
+        '<div id="' + id + '"></div>' );
+        $('#' + id).css({
+          "border-style": "solid",
+          "box-sizing": "border-box",
+          "float": "left"
+        });
+        positionRenders();
+      } else {
+        alert('Reached maximum number of renders allow which is 4. You must drag a render out ' +
+         'of the viewer window and drop it into the thumbnails bar to make a render available');
+      }
+    }
   }
+/*,
+    change: function(event, ui) {
+      if(ui.sender) newList = ui.placeholder.parent().parent();
+  }*/
 });
 
 
-// ui-droppable CSS class is by default added to the element
-$('#viewthumbnail').droppable ({
-  scope: 'viewthumbnail', // restrict dropping only for draggable items that have the same scope str
+$('#viewrenders').sortable({
+  cursor: 'pointer',
+  containment: '#viewercontainer', // within which the displacement takes place
+  helper: 'clone',
+  //zIndex: 999,
+  appendTo: '#viewthumbnail',
+  connectWith: ".sortable",
+  dropOnEmpty: true,
 
-  // hoverClass: string representing one or more CSS classes to be added  when an accepted
-  // element moves into it
-
-  //event handlers
-  // ui.helper is the jQuery obj of the dropped elem
-  // ui.draggable is the jQuery object of the clicked elem (but not necessarily the elem that moves)
-  drop: function(evt, ui) {
-    var id = $(ui.draggable).attr("id");
-    $(ui.draggable).remove();
-    $('#' + id.replace("viewrender2D", "img")).css({ display:"block" });
-    --numRenderDivs;
-    positionRenders();
-  }
-
-}).sortable();
-
-function makeRenderDraggable(renderID) {
-
-  $('#' + renderID).draggable ({
-    cursor: 'pointer',
-    //scroll: false,
-    scope: 'viewthumbnail', // restrict drop only on items that have the same scope str
-    revert: 'invalid', // returns if dropped on an element that does not accept it
-    axis: 'x', // displacement only possible in x (horizontal) direction
-    containment: '#viewercontainer', // within which the displacement takes place
-    helper: 'clone',
-    //zIndex: 999,
-    appendTo: '#viewthumbnail',
-
-    //event handlers
-    // ui.helper is the jQuery obj of the dragged elem
-    start: function(evt, ui) {
-      var jqTh = $('#' + renderID.replace("viewrender2D", "img"));
-      var h = jqTh.css("height");
-      var w = jqTh.css("width");
-      ui.helper.css( {height: h, width: w} );
-    },
-
-    drag: function(evt, ui) {
-
-    },
-
-    stop: function(evt, ui) {
-      //this.style.display="";
-    }
-  });
-
-};
-
-
-
-// ui-droppable CSS class is by default added to the element
-$('#viewrenders').droppable({
-  scope: 'viewrenders', // restrict dropping only for draggable items that have the same scope str
-
-  // hoverClass: string representing one or more CSS classes to be added  when an accepted
-  // element moves into it
-
-  //event handlers
-  // ui.helper is the jQuery obj of the dropped elem
-  // ui.draggable is the jQuery object of the clicked elem (but not necessarily the elem that moves)
-  drop: function(evt, ui) {
-    if (numRenderDivs<4) {
-      ++numRenderDivs;
-      var id = 'viewrender2D' + $(ui.draggable).css({ display:"none"}).attr("id").replace("img","");
-      $('#viewrenders').append(
-      '<div id="' + id + '"></div>' );
-      $('#' + id).css({
-        "border-style": "solid",
-        "box-sizing": "border-box",
-        "float": "left"
-      });
-      makeRenderDraggable(id);
+  beforeStop: function(event, ui) {
+    if (ui.placeholder.parent().attr("id") === "viewthumbnail") {
+      $(this).sortable("cancel");
+      var id = ui.item.attr("id");
+      $(ui.item).remove();
+      $('#' + id.replace("viewrender2D", "img")).css({ display:"block" });
+      --numRenderDivs;
       positionRenders();
-    } else {
-      alert('Reached maximum number of renders allow which is 4. You must drag a render out ' +
-       'of the viewer window and drop it into the thumbnails bar to make a render available');
     }
-
-  //  .draggable ("disable")
-  //  .css ({ opacity : 1 });
   }
+  /*receive: function(event, ui) {
+
+  }*/
 
 }).css({ float: "left" });
 
