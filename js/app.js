48262362e0db261932637a40cf2b266204af9c37
Jorge <jbernal0019@yahoo.es>
2015-03-03 15:30:52 -0500

    mi2b2 demo

---
+++ js/app.js
@@ -0,0 +1,272 @@
+/**
+ * mi2b2 app
+ *
+ * This app reads a directory tree from a directory picker/dropzone button (chrome)
+ * or multiple neuroimage files in the same directory (other browsers) and pass
+ * them to a Viewer object for visualization and collaboration. DICOM files are
+ * previously sorted by patientID, studyInstanceUID, seriesInstanceUID using
+ * chafey's dicomParser: https://github.com/chafey/dicomParser.
+ */
+
+// Provide a namespace
+var app = app || {};
+
+  app.App = function() {
+
+    // Init jQuery UI tabs
+    $('#tabs').tabs();
+    $('#tabs').tabs("disable", 1);
+
+    // Multidimensional associative array with ordered DICOM files
+    this._dcmData = {};
+    // Number of DICOM files
+    this._numDicoms = 0;
+    // Number of files that are not DICOMs
+    this._numNotDicoms = 0;
+    // Source data array for the Viewer object
+    this._imgFileArr = [];
+    // Total number of files
+    this._numFiles = 0;
+    // Associative array of thumbnail image files
+    this._thumbnails = {};
+    // Viewer object
+    this.view = null;
+
+    var self = this;
+
+    // Event handler for the directory loader button
+    var dirBtn = document.getElementById('dirbtn');
+    dirBtn.onchange = function(e) {
+      var files = e.target.files;
+      var fileObj;
+
+      self._numFiles = files.length;
+
+      for (var i=0; i<self._numFiles; i++) {
+        fileObj = files[i];
+        if ('webkitRelativePath' in fileObj) {
+          fileObj.fullPath = fileObj.webkitRelativePath;
+        } else if (!('fullPath' in fileObj)) {
+          fileObj.fullPath = fileObj.name;
+        }
+        self.add(fileObj);
+      }
+    };
+
+    // Event handlers for the dropzone
+    var dropzone = document.getElementById('directoryselection');
+
+    dropzone.ondragenter = function(e) {
+      e.preventDefault();
+    };
+
+    dropzone.ondragover = function(e) {
+      e.preventDefault();
+    };
+
+    dropzone.ondrop = function(e) {
+      var files = [];
+      var fileObj;
+      var i;
+
+      e.preventDefault();
+      if (!e.dataTransfer.items) {
+
+        // browser is not chrome
+
+        if(e.dataTransfer.files){
+          files = e.dataTransfer.files;
+          self._numFiles = files.length;
+          for (i=0; i<self._numFiles; i++) {
+            fileObj = files[i];
+            if (!('fullPath' in fileObj)) {
+              fileObj.fullPath = fileObj.name;
+            }
+            self.add(fileObj);
+          }
+        } else {
+          alert('Unsuported browser');
+        }
+        return;
+      }
+
+      // chrome browser
+
+      var length = e.dataTransfer.items.length;
+      // array to control when the entire tree has been read. This happens when
+      // all it's entries are different from zero
+      var hasBeenRead = [];
+
+      function readFiles(entry) {
+        var pos = hasBeenRead.length;
+        hasBeenRead[pos] = 0;
+
+        function readingDone() {
+          var i;
+
+          hasBeenRead[pos] = 1;
+          for (i=0; i<hasBeenRead.length; i++) {
+            if (hasBeenRead[i] === 0) {
+              break;
+            }
+          }
+          if (i >= hasBeenRead.length) {
+            self._numFiles = files.length;
+            for (i=0; i<self._numFiles; i++) {
+              self.add(files[i]);
+            }
+          }
+        }
+
+        if (entry.isFile) {
+          entry.file(function(file){
+            file.fullPath = entry.fullPath;
+            files.push(file);
+            readingDone();
+          });
+        } else if (entry.isDirectory) {
+          var dirReader = entry.createReader();
+          dirReader.readEntries(function(entries){
+            var idx = entries.length; //manage empty dir
+            while (idx--) { //read last entry until all have been read
+              readFiles(entries[idx]);
+            }
+            readingDone();
+          });
+        }
+      }
+
+      for (i = 0; i<length; i++) {
+        readFiles(e.dataTransfer.items[i].webkitGetAsEntry());
+      }
+
+    };
+
+
+  };
+
+  /**
+   * Add file into internal data structures
+   *
+   * @param {Object} HTML5 File object.
+   */
+  app.App.prototype.add = function(fileObj) {
+    var path = fileObj.fullPath;
+    var imgType = viewer.Viewer.imgType(fileObj);
+
+    if (imgType === 'dicom') {
+      // parse the dicom file
+      this.parseDicom(fileObj);
+    } else {
+      if (imgType === 'thumbnail') {
+        // save thumbnail file in an associative array
+        // array keys are the full path with the extension trimmed
+        this._thumbnails[path.substring(0, path.lastIndexOf('.'))] = fileObj;
+      } else if (imgType !== 'unsupported') {
+        // push fibers, meshes and volumes into this._imgFileArr
+        this._imgFileArr.push({
+          'baseUrl': path.substring(0, path.lastIndexOf('/') + 1),
+          'imgType': imgType,
+          'files': [fileObj]
+        });
+      }
+      ++this._numNotDicoms;
+      // if all files have been added then create view
+      if (this._numDicoms + this._numNotDicoms === this._numFiles) {
+        this.createView();
+      }
+    }
+
+  };
+
+  /**
+   * Parse and organize DICOM files by patientID, studyInstanceUID,
+   * seriesInstanceUID, sopInstanceUID
+   *
+   * @param {Object} HTML5 File object.
+   */
+  app.App.prototype.parseDicom = function(fileObj) {
+    var reader = new FileReader();
+    var self = this;
+
+    reader.onload = function() {
+      var arrayBuffer = reader.result;
+      // Here we have the file data as an ArrayBuffer.  dicomParser requires as input a
+      // Uint8Array so we create that here
+      var byteArray = new Uint8Array(arrayBuffer);
+      // Invoke the parseDicom function and get back a DataSet object with the contents
+      var dataSet, patientID, studyInstanceUID, seriesInstanceUID, sopInstanceUID;
+      var path = fileObj.fullPath;
+      var baseUrl = path.substring(0, path.lastIndexOf('/') + 1);
+      var filename = fileObj.name;
+
+      try {
+        dataSet = dicomParser.parseDicom(byteArray);
+        // Access any desire property using its tag
+        patientID = dataSet.string('x00100020');
+        studyInstanceUID = dataSet.string('x0020000d');
+        seriesInstanceUID = dataSet.string('x0020000e');
+        sopInstanceUID = dataSet.string('x00080018');
+        if (!self._dcmData[patientID]) {
+          self._dcmData[patientID] = {};
+        }
+        if (!self._dcmData[patientID][studyInstanceUID]) {
+          self._dcmData[patientID][studyInstanceUID] = {};
+        }
+        if (!self._dcmData[patientID][studyInstanceUID][seriesInstanceUID]) {
+          self._dcmData[patientID][studyInstanceUID][seriesInstanceUID] = {};
+          self._dcmData[patientID][studyInstanceUID][seriesInstanceUID]['baseUrl'] = baseUrl;
+          self._dcmData[patientID][studyInstanceUID][seriesInstanceUID]['files'] = [];
+        }
+        if (!self._dcmData[patientID][studyInstanceUID][seriesInstanceUID][sopInstanceUID]) {
+          self._dcmData[patientID][studyInstanceUID][seriesInstanceUID]['files'].push(fileObj);
+          self._dcmData[patientID][studyInstanceUID][seriesInstanceUID][sopInstanceUID] = filename;
+        }
+        ++self._numDicoms;
+        // if all files have been added then create view
+        if (self._numDicoms + self._numNotDicoms === self._numFiles) {
+          self.createView();
+        }
+      } catch(err) {
+        alert('File ' + path + ' Error - ' + err);
+      }
+    };
+    reader.readAsArrayBuffer(fileObj);
+  };
+
+  /**
+   * Create Viewer object
+   */
+  app.App.prototype.createView = function() {
+    var path;
+
+    // Push ordered DICOMs into this._imgFileArr
+    for (var patient in this._dcmData) {
+      for (var study in this._dcmData[patient]) {
+        for (var series in this._dcmData[patient][study]) {
+          this._imgFileArr.push({
+            'baseUrl': this._dcmData[patient][study][series]['baseUrl'],
+            'imgType': 'dicom',
+            'files': this._dcmData[patient][study][series]['files']
+          });
+        }
+      }
+    }
+
+    // Add thumbnail images
+    // For DICOM we assume the thumbnail has the same name as the first DICOM file
+    for (var i = 0; i < this._imgFileArr.length; i++) {
+      path = this._imgFileArr[i].files[0].fullPath;
+      this._imgFileArr[i].thumbnail = this._thumbnails[path.substring(0, path.lastIndexOf('.'))];
+    }
+    // Instantiate a new Viewer object
+    if (this.view) {
+      this.view.destroy();
+      this.view = null;
+    }
+    this.view = new viewer.Viewer(this._imgFileArr, 'viewercontainer');
+    this.view.addThumbnailBar();
+    this.view.addToolBar();
+    $('#tabs').tabs("enable", 1).tabs("option", "active", 1);
+    //app.view.connect(feedID);
+  };
---
+++ js/viewer.js
@@ -0,0 +1,489 @@
+/**
+ * This module takes care of all image visualization and user interface.
+ *
+ * An object of the Viewer class expects an array of image file objects where
+ * each object contains the following properties:
+ * -baseUrl: String ‘directory/containing/the/files’
+ * -thumbnail: HTML5 File object (for a thumbnail image)
+ * -imgType: String neuroimage type. Any of the possible values returned by viewer.Viewer.imgType
+ * -files: Array of HTML5 File objects (it contains a single file for formats other than DICOM)
+ */
+
+// Provide a namespace
+var viewer = viewer || {};
+
+  viewer.Viewer = function(imgFileArr, containerID) {
+
+    this.version = 0.0;
+    // array of image file objects
+    this.imgFileArr = imgFileArr;
+    // assign an id to each array elem
+    for (var i=0; i<this.imgFileArr.length; i++) {
+      this.imgFileArr[i].id = i;
+    }
+    // viewer container's ID
+    this.wholeContID = containerID;
+    // tool bar container's ID
+    this.toolContID = 'viewtoolbar';
+    // thumbnail container's ID
+    this.thumbnailContID = 'viewthumbnailbar';
+    // renderers container's ID
+    this.rendersContID =  'viewrenders';
+    // 2D renderers
+    this.renders2D = [];
+    // maximum number of renderers
+    this.maxNumOfRenders = 4;
+    // current number of renderers
+    this.numOfRenders = 0;
+    // insert initial html
+    this._initInterface();
+
+  };
+
+
+  /**
+   * Append initial html interface to the viewer container.
+   */
+  viewer.Viewer.prototype._initInterface = function() {
+    var self = this;
+
+    // Initially the interface only contains the renderers' container which in turn contains a
+    // single renderer that loads and displays the first volume in this.imgFileArr
+
+    $('#' + this.wholeContID).css({
+      "position": "relative",
+      "margin": 0,
+      "-webkit-box-sizing": "border-box",
+      "-moz-box-sizing": "border-box",
+      "box-sizing": "border-box"
+      }).append('<div id="' + this.rendersContID + '"></div>' );
+
+    $('#' + this.rendersContID).addClass("sortable");
+
+    // jQuery UI options object for sortable elems
+    // ui-sortable CSS class is by default added to the element
+    // element being moved is assigned the ui-sortable-helper class
+    var sort_opts = {
+      cursor: 'pointer',
+      containment: '#' + this.wholeContID, // CSS selector within which elem the displacement is restricted
+      helper: 'clone', // We actually move a clone of the elem rather than the elem itself
+      appendTo: '#' + this.thumbnailContID, // CSS selector given the receiver container for the moved clone
+      connectWith: ".sortable",
+      dropOnEmpty: true,
+
+      //event handlers
+      start: function(event, ui) {
+        var thWidth =  $('.view-thumbnail-img').css("width");
+        var thHeight = $('.view-thumbnail-img').css("height");
+        ui.helper.css({ width: thWidth, height: thHeight });
+      },
+
+      beforeStop: function(event, ui) {
+        if (ui.placeholder.parent().attr("id") === self.thumbnailContID) {
+          $(this).sortable("cancel");
+          var id = ui.item.attr("id");
+          // display the dropped renderer's thumbnail
+          $('#' + id.replace("viewrender2D", "viewth")).css({ display:"block" });
+          self.remove2DRender(id);
+        }
+      }
+    };
+
+    // make the renderers container sortable
+    $('#' + this.rendersContID).sortable(sort_opts);
+
+    // load and render the first volume in the list
+    for (var i=0; i<this.imgFileArr.length; i++) {
+      if (this.imgFileArr[i].imgType==='vol' || this.imgFileArr[i].imgType==='dicom') {
+        this.add2DRender(this.imgFileArr[i], 'Z');
+        break;
+      }
+    }
+
+  };
+
+  /**
+   * Create and add 2D renderer with a loaded volume to the UI.
+   *
+   * @param {Oject} Image file object.
+   * @param {String} X, Y or Z orientation.
+   */
+  viewer.Viewer.prototype.add2DRender = function(imgFileObj, orientation) {
+    var render, vol, containerID;
+    var filedata = [];
+    var numFiles = 0;
+
+    // append render div to the renderers container
+    containerID = 'viewrender2D' + imgFileObj.id;
+    $('#' + this.rendersContID).append('<div id="' + containerID + '"></div>');
+    $('#' + containerID).addClass("view-render");
+
+    // rearrange layout
+    ++this.numOfRenders;
+    this.positionRenders();
+
+    // create xtk objects
+    render = this.create2DRender(containerID, orientation);
+    vol = this.createVolume(imgFileObj);
+
+    // add xtk 2D renderer to the list of current UI renders
+    this.renders2D.push(render);
+
+    // define function to read a file into filedata array
+    function readFile(fileObj, pos) {
+      var reader = new FileReader();
+
+      reader.onload = function() {
+        filedata[pos] = reader.result;
+        ++numFiles;
+
+        if (numFiles===imgFileObj.files.length) {
+          vol.filedata = filedata;
+          render.add(vol);
+          // start the rendering
+          render.render();
+          viewer.documentRepaint();
+        }
+      };
+
+      reader.readAsArrayBuffer(fileObj);
+    }
+
+    // read all neuroimage files in imgFileObj.files
+    for (var i=0; i<imgFileObj.files.length; i++) {
+      readFile(imgFileObj.files[i], i);
+    }
+
+  };
+
+  /**
+   * Remove 2D renderer from the UI.
+   *
+   * @param {String} renderer's container.
+   */
+  viewer.Viewer.prototype.remove2DRender = function(containerID) {
+
+    // find and destroy xtk objects and remove the renderer's div from the UI
+    for (var i=0; i<this.renders2D.length; i++) {
+      if (this.renders2D[i].q.id === containerID) {
+        this.renders2D[i].destroy();
+        this.renders2D.splice(i, 1);
+        $('#' + containerID).remove();
+        --this.numOfRenders;
+        this.positionRenders();
+        viewer.documentRepaint();
+        break;
+      }
+    }
+
+  };
+
+  /**
+   * Create a 2D renderer object.
+   *
+   * @param {String} container id.
+   * @param {String} X, Y or Z orientation.
+   */
+  viewer.Viewer.prototype.create2DRender = function(containerID, orientation) {
+    var render;
+
+    // create xtk objects
+    render = new X.renderer2D();
+    render.container = containerID;
+    render.bgColor = [0.2, 0.2, 0.2];
+    render.orientation = orientation;
+    render.init();
+    return render;
+  };
+
+  /**
+   * Create a volume object.
+   *
+   * @param {Object} image file object
+   */
+  viewer.Viewer.prototype.createVolume = function(imgFileObj) {
+    var fileNames = [];
+    var vol;
+
+    for (var i=0; i<imgFileObj.files.length; i++) {
+      fileNames[i] = imgFileObj.files[i].name;
+    }
+    vol = new X.volume();
+    vol.reslicing = 'false';
+    vol.file = fileNames.sort().map(function(str) {
+      return imgFileObj.baseUrl + str;});
+    return vol;
+  };
+
+  /**
+   * Rearrange renderers in the UI layout.
+   */
+  viewer.Viewer.prototype.positionRenders = function() {
+    var jqRenders = $('div.view-render');
+
+    switch(this.numOfRenders) {
+      case 1:
+        jqRenders.css({
+          width: "100%",
+          height: "100%",
+          top: 0,
+          left: 0
+        });
+      break;
+
+      case 2:
+        jqRenders.css({
+          width: "50%",
+          height: "100%",
+          top: 0,
+          left:0
+        });
+        jqRenders[1].style.left = "50%";
+      break;
+
+      case 3:
+        jqRenders.css({
+          width: "50%",
+          height: "50%",
+        });
+        jqRenders[0].style.top = 0;
+        jqRenders[0].style.left = 0;
+        jqRenders[1].style.top = 0;
+        jqRenders[1].style.left = "50%";
+        jqRenders[2].style.top = "50%";
+        jqRenders[2].style.left = 0;
+        jqRenders[2].style.width = "100%";
+      break;
+
+      case 4:
+        jqRenders.css({
+          width: "50%",
+          height: "50%",
+        });
+        jqRenders[0].style.top = 0;
+        jqRenders[0].style.left = 0;
+        jqRenders[1].style.top = 0;
+        jqRenders[1].style.left = "50%";
+        jqRenders[2].style.top = "50%";
+        jqRenders[2].style.left = 0;
+        jqRenders[3].style.top = "50%";
+        jqRenders[3].style.left = "50%";
+      break;
+    }
+  };
+
+  /**
+   * Create and add toolbar to the viewer container.
+   */
+  viewer.Viewer.prototype.addToolBar = function() {
+
+    if ($('#' + this.toolContID).length) {
+      return; // toolbar already exists
+    }
+    // append thumbnail divs to the whole container
+    $('#' + this.wholeContID).append(
+      '<div id="' + this.toolContID + '"><div></div></div>'
+    );
+
+    // make space for the toolbar
+    var jqToolCont = $('#' + this.toolContID);
+    var rendersTopEdge = parseInt(jqToolCont.css("top")) + parseInt(jqToolCont.css("height")) + 5;
+    $('#' + this.rendersContID).css({ height: "calc(100% - " + rendersTopEdge + "px)" });
+    if ($('#' + this.thumbnailContID).length) {
+      // there is a thumbnail bar so make space for it
+      var jqThCont = $('#' + this.thumbnailContID);
+      var toolLeftEdge = parseInt(jqThCont.css("left")) + parseInt(jqThCont.css("width")) + 5;
+      jqToolCont.css({ width: "calc(100% - " + toolLeftEdge + "px)" });
+    }
+  };
+
+  /**
+   * Create and add thumbnail bar to the viewer container.
+   */
+  viewer.Viewer.prototype.addThumbnailBar = function() {
+    var self = this;
+
+    if (this.imgFileArr.length<2){
+      return; // a single (or none) file doesn't need a thumbnail bar
+    }
+
+    if ($('#' + this.thumbnailContID).length){
+      return; // thumbnail bar already exists
+    }
+
+    // define function to read the thumbnails' url so it can be assigned to the src of <img>
+    function readThumbnailUrl(imgFileObj, callback) {
+      var fileObj = imgFileObj.thumbnail;
+      var reader = new FileReader();
+
+      reader.onload = function() {
+        callback(imgFileObj.id, fileObj.name, reader.result);
+      };
+
+      reader.readAsDataURL(fileObj);
+    }
+
+    // callback to append new img elem
+    function createImgElm(id, altText, url) {
+      if (url === undefined) {
+        url = ' ';
+      }
+      $('#' + self.thumbnailContID).append(
+          '<img id="viewth' + id + '" src="' + url + '" alt="' + altText.substr(-8) + '" title="' + altText + '">'
+      );
+      $('#viewth' + id).addClass("view-thumbnail-img");
+      // if there is a corresponding renderer already in the UI then hide this thumbnail image
+      if ($('#viewrender2D' + id).length) {
+        $('#viewth' + id).css({ display:"none" });
+      }
+    }
+
+    // append thumbnail div to the whole container
+    $('#' + this.wholeContID).append(
+      '<div id="' + this.thumbnailContID + '"></div>'
+    );
+    $('#' + this.thumbnailContID).addClass("sortable");
+
+    // make the thumbnails container sortable
+    var sort_opts = {
+      cursor: 'pointer',
+      containment: '#' + this.wholeContID,
+      helper: 'clone',
+      appendTo: '#' + this.rendersContID,
+      connectWith: ".sortable",
+      dropOnEmpty: true,
+
+      //event handlers
+      // beforeStop is called when the placeholder is still in the list
+      beforeStop: function(event, ui) {
+        if (ui.placeholder.parent().attr("id") === self.rendersContID) {
+          $(this).sortable("cancel");
+          if (self.numOfRenders < self.maxNumOfRenders) {
+            // a dropped thumbnail image disappears from thumbnail bar
+            var id = parseInt(ui.item.css({ display:"none" }).attr("id").replace("viewth",""));
+            // add a renderer to the UI
+            self.add2DRender(self.getImgFileObject(id), 'Z');
+          } else {
+            alert('Reached maximum number of renders allow which is 4. You must drag a render out ' +
+             'of the viewer window and drop it into the thumbnails bar to make a render available');
+          }
+        }
+      }
+    };
+
+    $('#' + this.thumbnailContID).sortable(sort_opts);
+
+    // load thumbnail images
+    var imgFileObj;
+    for (var i=0; i<this.imgFileArr.length; i++) {
+      imgFileObj = this.imgFileArr[i];
+      if (imgFileObj.thumbnail) {
+        readThumbnailUrl(imgFileObj, createImgElm);
+      } else {
+        createImgElm(imgFileObj.id, imgFileObj.files[0].name);
+      }
+    }
+
+    // make space for the thumbnail bar
+    var jqThCont = $('#' + this.thumbnailContID);
+    var rendersLeftEdge = parseInt(jqThCont.css("left")) + parseInt(jqThCont.css("width")) + 5;
+    $('#' + this.rendersContID).css({ width: "calc(100% - " + rendersLeftEdge + "px)" });
+    if ($('#' + this.toolContID).length) {
+      // there is a toolbar
+      $('#' + this.toolContID).css({ width: "calc(100% - " + rendersLeftEdge + "px)" });
+    }
+
+  };
+
+
+  /**
+   * Destroy all objects and remove html interface
+   */
+  viewer.Viewer.prototype.destroy = function() {
+
+    // destroy XTK renderers
+    for (var i=0; i<this.renders2D.length; i++) {
+      this.renders2D[i].destroy();
+    }
+    this.renders2D = [];
+
+    // remove html
+    $('#' + this.wholeContID).empty();
+  };
+
+  /**
+   * Return image file object given its id
+   *
+   * @param {Number} Integer number between 0 and this.imgFileArr.length-1.
+   */
+  viewer.Viewer.prototype.getImgFileObject = function(id) {
+    return this.imgFileArr[id];
+  };
+
+
+  /**
+   * Static method to determine if a File object is a supported neuroimage type.
+   * Return the type of the image: 'dicom', 'vol', 'fibers', 'mesh', 'thumbnail'
+   * or 'unsupported'
+   *
+   * @param {Object} HTML5 File object
+   */
+  viewer.Viewer.imgType = function(fileObj) {
+    var ext = {};
+    var type;
+
+    // dicom extensions
+    ext.DICOM = ['.dcm', '.ima', '.DCM', '.IMA'];
+    // volume extensions
+    ext.VOL = ['.mgh', '.mgz', '.nrrd', '.nii', '.nii.gz'];
+    // fibers extension is .trk
+    ext.FIBERS = ['.trk'];
+    // geometric model extensions
+    ext.MESH = ['obj', 'vtk', 'stl'];
+    // thumbnail extensions
+    ext.THUMBNAIL = ['png', 'gif', 'jpg'];
+
+    if (viewer.strEndsWith(fileObj.name, ext.DICOM)) {
+      type = 'dicom';
+    } else if (viewer.strEndsWith(fileObj.name, ext.VOL)) {
+      type = 'vol';
+    } else if (viewer.strEndsWith(fileObj.name, ext.FIBERS)) {
+      type = 'fibers';
+    } else if (viewer.strEndsWith(fileObj.name, ext.MESH)) {
+      type = 'mesh';
+    } else if (viewer.strEndsWith(fileObj.name, ext.THUMBNAIL)) {
+      type = 'thumbnail';
+    } else {
+      type = 'unsupported';
+    }
+
+    return type;
+  };
+
+  /**
+   * Module utility function. Return true if the string str ends with any of the
+   * specified suffixes in arrayOfStr otherwise return false
+   *
+   * @param {String} input string
+   * @param {Array} array of string suffixes
+   */
+  viewer.strEndsWith = function(str, arrayOfStr) {
+    var index;
+
+    for (var i=0; i<arrayOfStr.length; i++) {
+      index = str.lastIndexOf(arrayOfStr[i]);
+      if ((index !== -1) && ((str.length-index) === arrayOfStr[i].length)) {
+        return true;
+      }
+    }
+    return false;
+  };
+
+  /**
+   * Module utility function. Repaint the document
+   */
+  viewer.documentRepaint = function() {
+    var ev = document.createEvent('Event');
+    ev.initEvent('resize', true, true);
+    window.dispatchEvent(ev);
+  };
