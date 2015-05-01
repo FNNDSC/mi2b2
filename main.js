6fa1f2f64718535b214c064064a4d0a3512b7e0c
Jorge <jbernal0019@yahoo.es>
2015-05-01 16:10:22 -0400

    mi2b2 app is now AMD and uses viewerjs

---
+++ main.js
@@ -0,0 +1,25 @@
+require.config({
+  baseUrl: 'js/components',
+  paths: {
+    // The left side is the module ID, the right side is the path to the file relative
+    // to baseUrl (which is in turn relative to the directory of this config script).
+    // Also, the path should NOT include the '.js' file extension.
+    // This example tries to load jQuery from Google's CDN first and if failure then falls
+    // back to the local jQuery at jquery/dist/jquery.min.js relative to the baseUrl.
+    //
+    // Modules dicomParser, xtk and viewerjs are only needed in development mode. They are
+    // no longer needed after building the app.
+    jquery: ['https://ajax.googleapis.com/ajax/libs/jquery/1.11.2/jquery.min', 'jquery/dist/jquery.min'],
+    jquery_ui: ['https://ajax.googleapis.com/ajax/libs/jqueryui/1.11.2/jquery-ui.min', 'jquery-ui/jquery-ui.min'],
+    dicomParser: 'dicomParser/dist/dicomParser.min',
+    xtk: 'viewerjs/src/js/lib/xtk',
+    viewerjs: 'viewerjs/src/js/viewerjs',
+    mi2b2: '../mi2b2'
+  }
+});
+
+
+require(['mi2b2'], function(mi2b2) {
+  // Entry point
+  mi2b2.app = new mi2b2.App();
+});
