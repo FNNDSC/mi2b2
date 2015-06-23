4055f7c4a8e87309713d8d46844e6b7e72bc8e6c
Jorge <jbernal0019@yahoo.es>
2015-06-23 15:14:00 -0400

    mi2b2 gh-pages updated

--- main.js
+++ main.js
@@ -7,12 +7,16 @@ require.config({
     // This example tries to load jQuery from Google's CDN first and if failure then falls
     // back to the local jQuery at jquery/dist/jquery.min.js relative to the baseUrl.
     //
-    // Modules dicomParser, xtk and viewerjs are only needed in development mode. They are
-    // no longer needed after building the app.
+    // All JS modules are needed in development mode. However the only modules needed after
+    // building the app are jquery, jquery_ui and mi2b2.
     jquery: ['https://ajax.googleapis.com/ajax/libs/jquery/1.11.2/jquery.min', 'jquery/dist/jquery.min'],
     jquery_ui: ['https://ajax.googleapis.com/ajax/libs/jqueryui/1.11.2/jquery-ui.min', 'jquery-ui/jquery-ui.min'],
+    gapi: 'https://apis.google.com/js/api',
+    jszip: 'jszip/dist/jszip',
     dicomParser: 'dicomParser/dist/dicomParser.min',
     xtk: 'viewerjs/src/js/lib/xtk',
+    fmjs: 'fmjs/src/js/fmjs',
+    gcjs: 'gcjs/src/js/gcjs',
     viewerjs: 'viewerjs/src/js/viewerjs',
     mi2b2: '../mi2b2'
   }
