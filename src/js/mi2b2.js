aec7e1372a2d4b5ec434c91dd3ffdb7ac4a8f564
nicolasrannou <npm.rannou@gmail.com>
2016-01-13 23:49:56 +0100

    hook all atlases

--- src/js/mi2b2.js
+++ src/js/mi2b2.js
@@ -28,15 +28,113 @@ define(['utiljs', 'gcjs', 'viewerjs'], function(util, cjs, viewerjs) {
   mi2b2.App.prototype.init = function() {
     this.view.init();
     var imgFileArr = [];
+    //
+    // 0-1 weeks
     imgFileArr.push({
-      'url': 'http://www.googledrive.com/host/0B8u7h0aKnydhVWNSVEtlUlY0Vzg/nico.nii.gz'
+      'url': 'http://www.googledrive.com/host/0B8u7h0aKnydhcEV1Vy1RVnJJU2s/w0to1.nii.gz'
     });
     imgFileArr.push({
-      'url': 'http://www.googledrive.com/host/0B8u7h0aKnydhVWNSVEtlUlY0Vzg/nico.nii.json'
+      'url': 'http://www.googledrive.com/host/0B8u7h0aKnydhcEV1Vy1RVnJJU2s/w0to1.nii.jpg'
     });
     imgFileArr.push({
-      'url': 'http://www.googledrive.com/host/0B8u7h0aKnydhVWNSVEtlUlY0Vzg/nico.nii.jpg'
+      'url': 'http://www.googledrive.com/host/0B8u7h0aKnydhcEV1Vy1RVnJJU2s/w0to1.nii.json'
     });
+
+    //
+    // quarter 0
+    imgFileArr.push({
+      'url': 'http://www.googledrive.com/host/0B8u7h0aKnydhcGxEU3Bxd1VEa0k/q0.nii.gz'
+    });
+    imgFileArr.push({
+      'url': 'http://www.googledrive.com/host/0B8u7h0aKnydhcGxEU3Bxd1VEa0k/q0.nii.jpg'
+    });
+    imgFileArr.push({
+      'url': 'http://www.googledrive.com/host/0B8u7h0aKnydhcGxEU3Bxd1VEa0k/q0.nii.json'
+    });
+
+    //
+    // quarter 1
+    imgFileArr.push({
+      'url': 'http://www.googledrive.com/host/0B8u7h0aKnydhajlIcWxJUGlJQk0/q1.nii.gz'
+    });
+    imgFileArr.push({
+      'url': 'http://www.googledrive.com/host/0B8u7h0aKnydhajlIcWxJUGlJQk0/q1.nii.jpg'
+    });
+    imgFileArr.push({
+      'url': 'http://www.googledrive.com/host/0B8u7h0aKnydhajlIcWxJUGlJQk0/q1.nii.json'
+    });
+
+    //
+    // quarter 2
+    imgFileArr.push({
+      'url': 'http://www.googledrive.com/host/0B8u7h0aKnydhTXFwM08yNENWdjQ/q2.nii.gz'
+    });
+    imgFileArr.push({
+      'url': 'http://www.googledrive.com/host/0B8u7h0aKnydhTXFwM08yNENWdjQ/q2.nii.jpg'
+    });
+    imgFileArr.push({
+      'url': 'http://www.googledrive.com/host/0B8u7h0aKnydhTXFwM08yNENWdjQ/q2.nii.json'
+    });
+
+    // quarter 3
+    imgFileArr.push({
+      'url': 'http://www.googledrive.com/host/0B8u7h0aKnydhNGlYcURmQ1FLWXM/q3.nii.gz'
+    });
+    imgFileArr.push({
+      'url': 'http://www.googledrive.com/host/0B8u7h0aKnydhNGlYcURmQ1FLWXM/q3.nii.jpg'
+    });
+    imgFileArr.push({
+      'url': 'http://www.googledrive.com/host/0B8u7h0aKnydhNGlYcURmQ1FLWXM/q3.nii.json'
+    });
+
+    //
+    // year 1 to 2
+    imgFileArr.push({
+      'url': 'http://www.googledrive.com/host/0B8u7h0aKnydhQjgwYlJPSmdSQzQ/y1to2.nii.gz'
+    });
+    imgFileArr.push({
+      'url': 'http://www.googledrive.com/host/0B8u7h0aKnydhQjgwYlJPSmdSQzQ/y1to2.nii.jpg'
+    });
+    imgFileArr.push({
+      'url': 'http://www.googledrive.com/host/0B8u7h0aKnydhQjgwYlJPSmdSQzQ/y1to2.nii.json'
+    });
+
+    //
+    // year 3 to 4
+    imgFileArr.push({
+      'url': 'http://www.googledrive.com/host/0B8u7h0aKnydhRVhURm41VWtlVDg/y3to4.nii.gz'
+    });
+    imgFileArr.push({
+      'url': 'http://www.googledrive.com/host/0B8u7h0aKnydhRVhURm41VWtlVDg/y3to4.nii.jpg'
+    });
+    imgFileArr.push({
+      'url': 'http://www.googledrive.com/host/0B8u7h0aKnydhRVhURm41VWtlVDg/y3to4.nii.json'
+    });
+
+    //
+    // year 4 to 5
+    imgFileArr.push({
+      'url': 'http://www.googledrive.com/host/0B8u7h0aKnydhU08wY0VzWnByRHM/y4to5.nii.gz'
+    });
+    imgFileArr.push({
+      'url': 'http://www.googledrive.com/host/0B8u7h0aKnydhU08wY0VzWnByRHM/y4to5.nii.jpg'
+    });
+    imgFileArr.push({
+      'url': 'http://www.googledrive.com/host/0B8u7h0aKnydhU08wY0VzWnByRHM/y4to5.nii.json'
+    });
+
+    //
+    // year 5 to 6
+    imgFileArr.push({
+      'url': 'http://www.googledrive.com/host/0B8u7h0aKnydhUFhBUlVVdFM0Zlk/y5to6.nii.gz'
+    });
+    imgFileArr.push({
+      'url': 'http://www.googledrive.com/host/0B8u7h0aKnydhUFhBUlVVdFM0Zlk/y5to6.nii.jpg'
+    });
+    imgFileArr.push({
+      'url': 'http://www.googledrive.com/host/0B8u7h0aKnydhUFhBUlVVdFM0Zlk/y5to6.nii.json'
+    });
+    
     // load atlases
     this.view.addData(imgFileArr);
   };
