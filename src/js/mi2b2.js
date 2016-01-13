4ac5d0b12eb849bd273d1e6c85016f77a59cfbeb
nicolasrannou <npm.rannou@gmail.com>
2016-01-13 22:08:14 +0100

    first working version of preset data

--- src/js/mi2b2.js
+++ src/js/mi2b2.js
@@ -28,20 +28,14 @@ define(['utiljs', 'gcjs', 'viewerjs'], function(util, cjs, viewerjs) {
   mi2b2.App.prototype.init = function() {
     this.view.init();
     var imgFileArr = [];
-    // create fake file
-    // -id: Integer id
-    //  *  -baseUrl: String ‘directory/containing/the/files’
-    //  *  -imgType: String neuroimage type. Any of the possible values returned by rendererjs.Renderer.imgType
-    //  *  -files: Array of HTML5 File objects or custom file objects with properties:
-    //  *     -remote: a boolean indicating whether the file has not been read locally (with a filepicker)
-    //  *     -url the file's url
-    //  *     -cloudId: the id of the file in a cloud storage system if stored in the cloud
-    //  *     -name: file name
-    //  *  The files array contains a single file for imgType different from 'dicom' or 'dicomzip'
-    //  *  -thumbnail: Optional HTML5 File or custom file object (optional jpg file for a thumbnail image)
-    //  *  -json: Optional HTML5 or custom File object (optional json file with the mri info for imgType different from 'dicom')
     imgFileArr.push({
       'url': 'http://www.googledrive.com/host/0B8u7h0aKnydhVWNSVEtlUlY0Vzg/nico.nii.gz'
+    });
+    imgFileArr.push({
+      'url': 'http://www.googledrive.com/host/0B8u7h0aKnydhVWNSVEtlUlY0Vzg/nico.nii.json'
+    });
+    imgFileArr.push({
+      'url': 'http://www.googledrive.com/host/0B8u7h0aKnydhVWNSVEtlUlY0Vzg/nico.nii.jpg'
     });
     // load atlases
     this.view.addData(imgFileArr);
