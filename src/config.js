156df70061cd402ef42a2acbb4743919031de4ec
nicolasrannou <npm.rannou@gmail.com>
2016-02-18 10:13:48 +0100

    connect.hostname disappeared during merge

--- src/config.js
+++ src/config.js
@@ -1,14 +1,13 @@
 require.config({
-  baseUrl: '../',
   paths: {
-    jquery: ['https://ajax.googleapis.com/ajax/libs/jquery/1.11.2/jquery.min', 'jquery/dist/jquery.min'],
-    jquery_ui: ['https://ajax.googleapis.com/ajax/libs/jqueryui/1.11.2/jquery-ui.min', 'jquery-ui/jquery-ui.min'],
+    jquery: ['https://ajax.googleapis.com/ajax/libs/jquery/1.11.2/jquery.min', '../../jquery/dist/jquery.min'],
+    jquery_ui: ['https://ajax.googleapis.com/ajax/libs/jqueryui/1.11.2/jquery-ui.min', '../../jquery-ui/jquery-ui.min'],
   },
   packages:[
     // local packages
   {
     name: 'mi2b2Package', // used for mapping...
-    location: 'js',   // relative to base url
+    location: '../js',   // relative to base url
     main: 'mi2b2'
   }
   ]
